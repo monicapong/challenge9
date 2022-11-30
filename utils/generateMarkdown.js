2aaa6d2ea9e7505356efbbecfd2c6e65ba49092c
monicapong <111400935+monicapong@users.noreply.github.com>
2022-11-30 13:53:23 -0500

    added js code to render license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -31,9 +31,38 @@ function renderLicenseBadge(license) {
   };
 };
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Calls renderLicenseLink to return the license link of the user's license input
+function renderLicenseLink(license) {
+  // If user input is AGPL,
+  if (license === 'GNU Affero General Public License') {
+    // return link to more information on AGPL 
+    return `(https://choosealicense.com/licenses/AGPL)`;
+    // If user input is GPL,
+  } else if (license === 'GNU General Public License') {
+    // return link to more information on GPL
+    return `(https://choosealicense.com/licenses/GPL)`;
+    // If user input is LGPL,
+  } else if (license === 'GNU Lesser General Public License') {
+    // return link to more information on LGPL
+    return `(https://choosealicense.com/licenses/LGPL)`;
+    // If user input is MPL,
+  } else if (license === 'Mozilla Public License') {
+    // return link to more information on MPL
+    return `(https://choosealicense.com/licenses/MPL)`;
+    // If user input is Apache,
+  } else if (license === 'Apache License') {
+    // return link to more information on apache
+    return `(https://choosealicense.com/licenses/apache)`;
+    // If user input is MIT,
+  } else if (license === 'MIT License') {
+    // return link to more information on MIT
+    return `(https://choosealicense.com/licenses/MIT)`;
+  // Else, if there is no license,
+  } else {
+    // return an empty string
+    return '';
+  };
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
